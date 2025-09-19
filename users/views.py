8fa1d25c1dfb18aa32cf8e3c9d40651f897ad257
Eduardo Henrique <87464701+eduardohasantos@users.noreply.github.com>
2025-09-19 10:07:01 -0300

    User registration backend implementation completed
    Finished the core logic for user registration. Users can now be registered via the terminal, but the HTML for displaying errors is still pending.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        birthdate = request.POST.get('birthdate')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse username')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        return HttpResponse('usuário cadastrado com sucesso')
 
 def login(request):
     return render(request, 'login.html')

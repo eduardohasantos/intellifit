05d21d0df69eb7175cef2189dcd5338ff3b05c19
Eduardo Henrique <87464701+eduardohasantos@users.noreply.github.com>
2025-09-20 10:25:33 -0300

    Login page completed and dashboard implementation started
    Finished the login page with user validation. If the credentials are valid, the user is redirected to the dashboard. If someone tries to access the dashboard directly without logging in, they are redirected back to the login page.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "GET":
@@ -11,7 +14,6 @@ def register(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        birthdate = request.POST.get('birthdate')
 
         if User.objects.filter(email=email).exists():
             messages.add_message(request, constants.ERROR, "Email já cadastrado")
@@ -28,7 +30,28 @@ def register(request):
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
 
-        return HttpResponse('usuário cadastrado com sucesso')
+        messages.sucess(request, "Usuário cadastrado com sucesso!")
+        return redirect('login')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login_django(request, user)
+
+            return redirect('dashboard')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+            return render(request, 'login.html', {
+                'username': username,
+            })
+        
+@login_required(login_url = "/auth/login/")
+def dashboard(request):
+    return render(request, 'dashboard.html')

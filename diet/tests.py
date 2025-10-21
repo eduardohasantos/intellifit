159de85cacacefafbba657b49df6b31e1ce537dd
ArthurMatias57 <email_que_voce_usou_no_github>
2025-10-20 21:12:49 -0300

    Teste E2E completo

--- diet/tests.py
+++ diet/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-# Create your tests here.
+#Imports normais do selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ["user-data.json"]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        
+        username = "arthur"
+        password = "arthur"
+        
+        self.selenium.get(f"{self.live_server_url}/login/")
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys(username)
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys(password)
+        self.selenium.find_element(By.XPATH, '//button[@type="submit"]').click()
+
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
+    path('login/',  views.login, name="login"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('logout/', views.logout_view, name='logout'), 
     path('edit_account/', views.edit_account, name='edit_account'), 

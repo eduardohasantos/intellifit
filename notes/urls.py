0cfbeee313d3753fe37e4d20e6fc578c07653a54
ArthurMatias57 <email_que_voce_usou_no_github>
2025-11-10 09:00:19 -0300

    História com cenários favoráveis finalizada

--- notes/urls.py
+++ notes/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('create/', views.note_create, name='note_create'),
     path('<int:pk>/', views.note_detail, name='note_detail'),   
+    path('<int:pk>/edit', views.edit_notes, name='edit_notes'),
+    path('<int:pk>/delete', views.delete_notes, name='delete_notes')
 ]
+
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,9 @@
 # notes/views.py
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Note
+from notes.forms import editNotesForm
+from django.contrib import messages
 
 @login_required
 def note_list(request):
@@ -24,12 +25,46 @@ def note_create(request):
     
     return render(request, 'notes/note_form.html')
 
-# ADICIONE A FUNÇÃO ABAIXO
 @login_required
 def note_detail(request, pk):
-    # Busca a anotação pelo ID (pk) E garante que ela pertence ao usuário logado
     note = get_object_or_404(Note, pk=pk, user=request.user)
     context = {
         'note': note
     }
     return render(request, 'notes/note_detail.html', context)
+
+@login_required
+def edit_notes(request, pk):
+    selected_note = get_object_or_404(Note, pk=pk, user=request.user)
+    
+    if request.method == 'POST':
+        # Passa request.POST e a instância para o formulário
+        form = editNotesForm(request.POST, instance=selected_note)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Nota editada com sucesso!")
+            return redirect("notes:note_list")  # Nome da URL, não caminho do HTML
+    else:
+        # GET: Renderiza o formulário com os dados atuais
+        form = editNotesForm(instance=selected_note)
+    
+    context = {
+        'note': selected_note,
+        'form': form
+    }
+    return render(request, 'notes/edit_notes.html', context)
+
+@login_required
+def delete_notes(request, pk):
+    selected_note = get_object_or_404(Note, pk=pk, user=request.user)
+    
+    if request.method == 'POST':
+        selected_note.delete()
+        messages.success(request, "Anotação removida com sucesso!")
+        return redirect("notes:note_list")  # Nome da URL, não caminho do HTML
+    
+    context = {
+        'note': selected_note
+    }
+    return render(request, 'notes/delete_notes.html', context)
